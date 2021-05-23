0fc3755170518a39cb571eb050cd855221736ae6
J.Henrique <jhenriqueribeirodesa@gmail.com>
2021-05-22 22:32:29 -0300

    Estrutura inicial

---
+++ src/main/java/com/henrique3g/controle_veiculos/ControleDeVeiculosApplication.java
@@ -0,0 +1,13 @@
+package com.henrique3g.controle_veiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeVeiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henrique3g/controle_veiculos/ControleDeVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henrique3g.controle_veiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

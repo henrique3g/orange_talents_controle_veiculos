a1735ec980af44b6fece1d26dd241c588263cb37
J.Henrique <jhenriqueribeirodesa@gmail.com>
2021-05-27 16:22:48 -0300

    Add message error when user already exists, and save user with the repository

--- src/main/java/com/henrique3g/controle_veiculos/CreateUserController.java
+++ src/main/java/com/henrique3g/controle_veiculos/CreateUserController.java
@@ -1,17 +1,39 @@
 package com.henrique3g.controle_veiculos;
 
 import javax.validation.Valid;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class CreateUserController {
+    @Autowired
+    private UserRepository userRepository;
+
     @PostMapping("/users")
     public ResponseEntity<Void> createUser(@RequestBody @Valid CreateUserDto userData) {
+        boolean hasUserWithSameCpf = userRepository.findByCpf(userData.getCpf()).isPresent();
+        boolean hasUserWithSameEmail = userRepository.findByEmail(userData.getEmail()).isPresent();
+        if (hasUserWithSameCpf || hasUserWithSameEmail) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    getErrorMessage(hasUserWithSameCpf, hasUserWithSameEmail));
+        }
+        userRepository.save(userData.toUser());
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    private String getErrorMessage(boolean cpf, boolean email) {
+        String message = "User already exists with the";
+        if (cpf && !email) {
+            return message.concat(" cpf");
+        }
+        if (!cpf && email) {
+            return message.concat(" email");
+        }
+        return  message.concat(" cpf and the email");
     }
 }

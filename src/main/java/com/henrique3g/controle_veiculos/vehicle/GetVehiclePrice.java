ab8d5cb3312d7c873796527d3f55ddb99150e9e1
J.Henrique <jhenriqueribeirodesa@gmail.com>
2021-05-27 22:08:59 -0300

    Create getVehiclePrice service

---
+++ src/main/java/com/henrique3g/controle_veiculos/vehicle/GetVehiclePrice.java
@@ -0,0 +1,49 @@
+package com.henrique3g.controle_veiculos.vehicle;
+
+import java.time.Year;
+import java.util.Optional;
+import com.henrique3g.controle_veiculos.vehicle.VehicleApiClient.ResponseVehicle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GetVehiclePrice {
+    @Autowired
+    private VehicleApiClient vehicleClient;
+
+    public Optional<Double> execute(String brand, String model, Year year) {
+        Optional<Integer> brandId = getBrandId(brand);
+        if (!brandId.isPresent()) {
+            return Optional.empty();
+        }
+        Optional<Integer> modelId = getModelId(brandId.get(), model);
+        if (!modelId.isPresent()) {
+            return Optional.empty();
+        }
+        Optional<String> yearId = getYearId(brandId.get(), modelId.get(), year);
+        if (!yearId.isPresent()) {
+            return Optional.empty();
+        }
+        ResponseVehicle vehicle =
+                vehicleClient.getVehicle(brandId.get(), modelId.get(), yearId.get());
+        return Optional.of(vehicle.getPrice());
+    }
+
+    private Optional<String> getYearId(Integer brandId, Integer modelId, Year year) {
+        return vehicleClient.getAllYearsOfAModel(brandId, modelId).stream()
+                .filter((item) -> item.getCode().contains(year.toString())).findFirst()
+                .map((item) -> item.getCode());
+    }
+
+    private Optional<Integer> getModelId(int brandId, String model) {
+        return vehicleClient.getAllModelsByBrand(brandId).modelos.stream()
+                .filter((item) -> item.getName().equalsIgnoreCase(model)).findFirst()
+                .map((item) -> item.getCode());
+    }
+
+    private Optional<Integer> getBrandId(String brand) {
+        return vehicleClient.getAllBrands().stream()
+                .filter((item) -> item.getName().equalsIgnoreCase(brand)).findFirst()
+                .map((item) -> item.getCode());
+    }
+}

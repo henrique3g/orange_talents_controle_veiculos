1596f3385dc445c7e9a013e2b56843650c338f75
J.Henrique <jhenriqueribeirodesa@gmail.com>
2021-05-27 21:07:09 -0300

    add cloud feign

--- src/main/java/com/henrique3g/controle_veiculos/ControleDeVeiculosApplication.java
+++ src/main/java/com/henrique3g/controle_veiculos/ControleDeVeiculosApplication.java
@@ -2,8 +2,10 @@ package com.henrique3g.controle_veiculos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ControleDeVeiculosApplication {
 
 	public static void main(String[] args) {

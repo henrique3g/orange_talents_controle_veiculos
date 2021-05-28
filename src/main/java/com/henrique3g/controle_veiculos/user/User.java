ccb708711b6f5be3037742d8f082701031ea25de
J.Henrique <jhenriqueribeirodesa@gmail.com>
2021-05-28 11:12:51 -0300

    add Vehicle relation and getters to all properties

--- src/main/java/com/henrique3g/controle_veiculos/user/User.java
+++ src/main/java/com/henrique3g/controle_veiculos/user/User.java
@@ -1,11 +1,14 @@
 package com.henrique3g.controle_veiculos.user;
 
 import java.time.LocalDate;
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import com.henrique3g.controle_veiculos.vehicle.Vehicle;
 import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
@@ -24,6 +27,9 @@ public class User {
     @DateTimeFormat(iso = ISO.DATE)
     private LocalDate birthdate;
 
+    @OneToMany(mappedBy = "user")
+    private List<Vehicle> vehicles;
+
     public User() {}
 
     public User(String name, String email, String cpf, LocalDate birthdate) {
@@ -31,5 +37,29 @@ public class User {
         this.email = email;
         this.cpf = cpf;
         this.birthdate = birthdate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public List<Vehicle> getVehicles() {
+        return vehicles;
     }
 }

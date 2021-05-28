e752750231a0f2e82e5188445e18f110b617de2f
J.Henrique <jhenriqueribeirodesa@gmail.com>
2021-05-28 20:37:14 -0300

    remove unused setters from Dtos

--- src/main/java/com/henrique3g/controle_veiculos/user/CreateUserDto.java
+++ src/main/java/com/henrique3g/controle_veiculos/user/CreateUserDto.java
@@ -32,31 +32,15 @@ public class CreateUserDto {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getCpf() {
         return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public LocalDate getBirthdate() {
         return birthdate;
-    }
-
-    public void setBirthdate(LocalDate birthdate) {
-        this.birthdate = birthdate;
     }
 }
--- src/main/java/com/henrique3g/controle_veiculos/vehicle/CreateVehicleDto.java
+++ src/main/java/com/henrique3g/controle_veiculos/vehicle/CreateVehicleDto.java
@@ -31,23 +31,11 @@ public class CreateVehicleDto {
         return model;
     }
 
-    public void setModel(String model) {
-        this.model = model;
-    }
-
     public String getBrand() {
         return brand;
     }
 
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
     public Year getYear() {
         return year;
-    }
-
-    public void setYear(Year year) {
-        this.year = year;
     }
 }

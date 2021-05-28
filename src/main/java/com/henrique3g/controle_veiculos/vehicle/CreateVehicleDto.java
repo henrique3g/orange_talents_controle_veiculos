277a9166a1f0be36a8b33bad1f7c955be2b28f2b
J.Henrique <jhenriqueribeirodesa@gmail.com>
2021-05-27 22:09:15 -0300

    Add CreateVehicleDto class

---
+++ src/main/java/com/henrique3g/controle_veiculos/vehicle/CreateVehicleDto.java
@@ -0,0 +1,51 @@
+package com.henrique3g.controle_veiculos.vehicle;
+
+import java.time.Year;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+public class CreateVehicleDto {
+    @NotNull
+    private Long userId;
+
+    @NotBlank
+    private String brand;
+
+    @NotBlank
+    private String model;
+
+    @NotNull
+    private Year year;
+
+    public Vehicle toVehicle(Double price) {
+        return new Vehicle(getBrand(), getModel(), getYear(), price);
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Year getYear() {
+        return year;
+    }
+
+    public void setYear(Year year) {
+        this.year = year;
+    }
+}
